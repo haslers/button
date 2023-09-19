9059a121f68e339fb93fef89d4e29b022494cc84
Stefanie Hasler <stefanie.hasler@codecentric.de>
2023-09-19 16:07:26 +0200

    first meaningful test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should render a button', () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  })
+
+})
