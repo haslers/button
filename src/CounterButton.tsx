a3b789cd6af86dcdba0eb86fce22b676600762b3
Stefanie Hasler <stefanie.hasler@codecentric.de>
2023-09-19 16:04:24 +0200

    first button

---
+++ src/CounterButton.tsx
@@ -0,0 +1,9 @@
+import {useState} from "react";
+
+
+const CounterButton = () => {
+    const [count, setCount] = useState(0);
+    return (<button onClick={() => setCount(count + 1)}>counter</button>);
+}
+
+export { CounterButton };
