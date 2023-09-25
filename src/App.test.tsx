c356985bd2345d1536bf2b9c20f348a039d50f65
Stefanie Hasler <stefanie.hasler@codecentric.de>
2023-09-25 15:49:38 +0200

    move app back to main directory

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {act, render, screen} from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 import * as axe from 'axe-core';
 
 describe('App', () => {
