759fc2b9a3de23f13d272505d18132e4f736be2a
Stefanie Hasler <stefanie.hasler@codecentric.de>
2023-09-25 19:54:20 +0200

    add some css; fix structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import {act, render, screen} from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import App from './App';
-import * as axe from 'axe-core';
 
 describe('App', () => {
-  it('should render a button', () => {
+  it('should render a the app', () => {
     render(<App />);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
   })
-
-  it('should display the value of the counter on the button', async () => {
-    render(<App />);
-    const button = screen.getByRole('button');
-    const display = screen.getByTestId("display")
-    expect(display.innerHTML).toEqual('Counter: 0')
-    await act(() => {
-      button.click();
-    })
-    expect(display.innerHTML).toEqual('Counter: 1')
-  })
-
-  it('should not throw accessibility errors', async() => {
-      const {container} = render(<App />);
-      const results = await axe.run(container)
-    console.log(results.violations)
-      expect(results.violations.length).toBe(0);
-    });
-
 })
